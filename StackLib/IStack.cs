48fda0e5d3973f8941ba53922d1840de431e6412
maxnorm <maxnorm@hotmail.com>
2022-09-20 17:21:00 -0400

    Ajustement/Formatage Code et Commentaire

--- StackLib/IStack.cs
+++ StackLib/IStack.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace StackLib;
 
+// Par Maxime Normandin
 public interface IStack<T> : IReadOnlyCollection<T>
 {
     bool IsEmpty { get; }
--- StackLib/StaticStack.cs
+++ StackLib/StaticStack.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace StackLib;
 
+// Par Maxime Normandin
 public class StaticStack<T> : IStaticStack<T>
 {
     private T[] array;
