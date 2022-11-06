f959e6cc3ad2ef0d69eccc2f3477d463ac543ed4
maxnorm <maxnorm@hotmail.com>
2022-11-06 18:21:54 -0500

    Fin Partie 2 LL - Implementation

---
+++ LinkedListConsole/Program.cs
@@ -0,0 +1,82 @@
+﻿using StackLib;
+using System.Text;
+using static System.Console;
+
+var linkedListStack = new LinkedListStack<string>();
+
+while (true)
+{
+    StringBuilder stringBPile = new StringBuilder();
+    stringBPile.Append("Pile LL ")
+               .Append(linkedListStack.Count)
+               .Append(":");
+
+    var enumerator = linkedListStack.GetEnumerator();
+
+    for (int i = 0; i < linkedListStack.Count; i++)
+    {
+        stringBPile.Append((i > 0 ? ", " : " ") + enumerator.Current);
+        enumerator.MoveNext();
+    }
+
+
+    WriteLine(stringBPile.ToString());
+
+    string entree = "";
+    while (entree == "")
+    {
+        Write("MN> ");
+        entree = ReadLine();
+    }
+
+    if (entree == "exit")
+        break;
+
+    string commande = entree.Split(" ")[0];
+    string item = entree.Substring(commande.Length);
+
+    switch (commande)
+    {
+        case "push":
+            try
+            {
+                if (item == "")
+                    WriteLine("    Erreur: Pousser quoi ???");
+                else
+                    linkedListStack.Push(item);
+            }
+            catch (InvalidOperationException ex)
+            {
+                WriteLine("    " + ex.GetType().Name + ": " + ex.Message);
+            }
+            break;
+
+        case "pop":
+            try
+            {
+                WriteLine("   " + linkedListStack.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                WriteLine("    " + ex.GetType().Name + ": " + ex.Message);
+            }
+            break;
+
+        case "peek":
+            try
+            {
+                WriteLine("   " +linkedListStack.Peek());
+            }
+            catch (InvalidOperationException ex)
+            {
+                WriteLine("    " + ex.GetType().Name + ": " + ex.Message);
+            }
+            break;
+
+        default:
+            WriteLine("    Commande invalide: " + commande);
+            break;
+    }
+    WriteLine();
+}
+WriteLine("Au revoir!");
--- StackLib/LinkedListStack.cs
+++ StackLib/LinkedListStack.cs
@@ -9,37 +9,77 @@ namespace StackLib
 {
     public class LinkedListStack<T> : IStack<T>
     {
+        private Noeud<T>? _racine = null;
+
+        private int _compteur = 0;
+
         public LinkedListStack() {
 
         }
 
-        public bool IsEmpty => throw new NotImplementedException();
+        public bool IsEmpty => _compteur == 0;
 
-        public int Count => throw new NotImplementedException();
+        public int Count => _compteur;
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            while (_racine != null)
+            {
+                T valeur = _racine.Item;
+                _racine = _racine.Suivant;
+                yield return valeur;
+            }
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if(IsEmpty)
+            {
+                throw new InvalidOperationException("Pile vide");
+            }
+
+            return _racine!.Item;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Pile vide");
+            }
+
+            var noeud = _racine;
+            _racine = _racine!.Suivant;
+            _compteur++;
+            return noeud!.Item;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _racine = new Noeud<T>(item, _racine);
+            _compteur++;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            while(_racine != null)
+            {
+                T valeur = _racine.Item;
+                _racine = _racine.Suivant;
+                yield return valeur;
+            }
+        }
+
+        class Noeud<Y>
+        {
+            public Y Item;
+            public Noeud<Y>? Suivant;
+            
+            public Noeud(Y item, Noeud<Y>? suivant = null)
+            {
+                Item = item;
+                Suivant = suivant;
+            }
         }
     }
 }
