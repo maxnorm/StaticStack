71e4305900d4ce20f4189f32d7058be08e9dc8d2
maxnorm <maxnorm@hotmail.com>
2022-11-06 20:01:11 -0500

    Fin LinkedListStack

--- StackLib/LinkedListStack.cs
+++ StackLib/LinkedListStack.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace StackLib
 {
+    // Maxime Normandin
     public class LinkedListStack<T> : IStack<T>
     {
         private Noeud<T>? _racine = null;
@@ -20,17 +16,6 @@ namespace StackLib
         public bool IsEmpty => _compteur == 0;
 
         public int Count => _compteur;
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            var copie = _racine;
-            while (copie != null)
-            {
-                T valeur = copie.Item;
-                copie = copie.Suivant;
-                yield return valeur;
-            }
-        }
 
         public T Peek()
         {
@@ -72,16 +57,28 @@ namespace StackLib
             }
         }
 
-        class Noeud<Y>
+        public IEnumerator<T> GetEnumerator()
         {
-            public Y Item;
-            public Noeud<Y>? Suivant;
-            
-            public Noeud(Y item, Noeud<Y>? suivant = null)
+            var copie = _racine;
+            while (copie != null)
             {
-                Item = item;
-                Suivant = suivant;
+                T valeur = copie.Item;
+                copie = copie.Suivant;
+                yield return valeur;
             }
+        }
+    }
+
+    // Maxime Normandin
+    class Noeud<T>
+    {
+        public T Item;
+        public Noeud<T>? Suivant;
+
+        public Noeud(T item, Noeud<T>? suivant = null)
+        {
+            Item = item;
+            Suivant = suivant;
         }
     }
 }
